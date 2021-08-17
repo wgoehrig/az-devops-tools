1b6c575101ae311045fd32da505015f90b27f6d0
KevinIsMyName <wu.kevin.2520@gmail.com>
2021-08-17 16:06:56 -0400

    Added verbosity to GETting webhooks, no args can be specified

--- src/svc-hooks/get.ts
+++ src/svc-hooks/get.ts
@@ -6,54 +6,59 @@ import { printTable } from "../utils/TableUtils";
 
 import chalk = require("chalk");
 
-export const command = "get <sth>";
+export const command = "get";
 export const desc = "Get a set of service hooks";
-export const builder = (yargs: import("yargs").Argv) => yargs
-  .positional("sth", {
-    describe: "Something to list service hooks of.",
-    type: "string"
+export const builder = (yargs: import("yargs").Argv) =>
+  yargs.option("v", {
+    // Allow the user to specify verbose mode
+    alias: "verbose",
+    demandOption: false,
+    describe: "Verbose logging",
+    type: "boolean",
   });
-export function handler(argv: any) { 
-  getServiceHooks(argv); 
+export function handler(argv: any) {
+  getServiceHooks(argv);
 }
 
 export async function getServiceHooks(argv: any) {
-  console.log(argv);
-
-  const spinner = startSpinner(chalk`Running {bold az pipelines variable-group list} ...`); 
-  // https://dev.azure.com/<org>/_apis/hooks/consumers
-  const checks = await runAzCommand([
-    "devops",
-    "invoke",
-    "--route-parameters",
-    "hubName=../../hooks/subscriptions",
-    "--area", "distributedtask",
-    "--resource", "hublicense",
-    "--api-version", "6.1-preview",
-    "--http-method", "GET",
-    "--only-show-errors",
-    "--in-file", JSON.stringify({
-      "contributionIds": [
-        "ms.vss-build-web.run-attempts-data-provider"
-      ],
-      // "dataProviderContext": {
-      //   "properties": {
-      //     "buildId": runId,
-      //     "stageName": id,
-      //     "sourcePage": {
-      //       "routeId": "ms.vss-build-web.ci-results-hub-route",
-      //       "routeValues": {
-      //         "project": project,
-      //       }
-      //     }
-      //   }
-      // }
-    })
-  ], { inFile: true });
+  /* GET request to https://dev.azure.com/<org>/_apis/hooks/consumers
+  SUPER DUPER HACKY
+  */
+  const spinner = startSpinner(chalk`Running {bold az devops invoke} ...`);
+  const checks = await runAzCommand(
+    [
+      "devops",
+      "invoke",
+      "--route-parameters",
+      "hubName=../../hooks/subscriptions",
+      "--area",
+      "distributedtask",
+      "--resource",
+      "hublicense",
+      "--api-version",
+      "6.1-preview",
+      "--http-method",
+      "GET",
+      "--only-show-errors",
+      "--in-file",
+      JSON.stringify({
+        contributionIds: ["ms.vss-build-web.run-attempts-data-provider"],
+      }),
+    ],
+    { inFile: true }
+  );
   spinner.stop();
 
-  // console.log(checks.value);
-  checks.value.map((i: any) => {
-    console.log(chalk `{gray ${i.actionDescription}} by {blue ${i.createdBy.uniqueName}} to POST {green ${i.consumerInputs.url}} on event: {cyan ${i.eventDescription}}`);
-  });
+  // Print the entire response is verbose mode.
+  if (argv.verbose == true) {
+    console.log(checks.value);
+  } else {
+    checks.value.map((i: any) => {
+      console.log(
+        chalk`{gray ${i.actionDescription}} by {blue ${i.createdBy.uniqueName}} on event {cyan ${i.eventDescription}} to POST {green ${i.consumerInputs.url}}`
+      );
+    });
+  }
+
+  console.log(argv.verbose);
 }
